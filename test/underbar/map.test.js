a579a3224a827e8098e0fa6a73c582e80b6aec62
interdigitize <interdigitize@users.noreply.github.com>
2017-05-11 20:36:29 -0700

    adds draft of the map function rebuild test

--- test/underbar/map.test.js
+++ test/underbar/map.test.js
@@ -2,6 +2,8 @@ const _ = require('../../underbar');
 
 describe('map()', () => {
   it('maps every numbers in an array of numbers to their square', () => {
-    // Your code goes here
+    const nums = [1, 2, 3, 4, 6];
+    const mappedArr = _.map(nums, function(el){ el * el});
+    expect(mappedArr).toEqual([1, 4, 9, 16, 36]);
   });
 });
